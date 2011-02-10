9c0b1104e8d517ada20d6b046d3895a97c4f8eb0
titus.appel <titus.appel@localhost>
2011-02-09 20:27:59 -0700

    Added multiple subject tracking

--- vicon/vicon_vrpn/src/vicon_vrpn_node.cpp
+++ vicon/vicon_vrpn/src/vicon_vrpn_node.cpp
@@ -1,8 +1,10 @@
 #include "ros/ros.h"
 #include "tf/transform_broadcaster.h"
-#include "vicon_vrpn/vrpn_Tracker.h"
+#include "vicon_vrpn/vrpn_subject.h"
 #include <stdlib.h>
 #include <stdio.h>
+
+#define MAX_SUBJECTS  100
 
 using namespace std;
 
@@ -14,56 +16,42 @@ class VRPNHandler
       n = nh;
       ros::NodeHandle n_private("~"); 
       n_private.param("host_addr", host_addr, string("Vicon"));
-    	n_private.param("tf_ref_frame", tf_ref_frame, string("/vicon_world"));     
-      n_private.param("subjects", subjects, string("Pioneer01"));
-      
-      address = subjects + "@" + host_addr;
+      n_private.param("tf_ref_frame", tf_ref_frame, string("/vicon_world"));     
 
-      // Open the tracker
-      tkr = new vrpn_Tracker_Remote(address.c_str());
-	    // Set up the tracker callback handler
-      tkr->register_change_handler(NULL, (vrpn_TRACKERCHANGEHANDLER)&VRPNHandler::handle_tracker);
+      if (!n_private.hasParam("subject_list"))
+        ROS_FATAL("Error: subject_list was not provided");
+
+      XmlRpc::XmlRpcValue subject_list;
+      n_private.getParam("subject_list", subject_list);
+      ROS_ASSERT(subject_list.getType() == XmlRpc::XmlRpcValue::TypeArray);
+
+      num_subjects = subject_list.size();
+
+      if (num_subjects > MAX_SUBJECTS)
+        ROS_FATAL("Tracking limit is %d subjects.", MAX_SUBJECTS);
+
+      for (int32_t i = 0; i < num_subjects; ++i) 
+      {
+        ROS_ASSERT(subject_list[i].getType() == XmlRpc::XmlRpcValue::TypeString);
+        subjects[i] = (string)subject_list[i];       
+        addresses[i] = subjects[i] + "@" + host_addr;
+        vrpn_subjects[i] = new VRPNSubject(subjects[i], addresses[i], tf_ref_frame, tf_broadcaster);
+        ROS_INFO("Tracking subject at: %s", ((string)addresses[i]).c_str());
+      } 
     }
 
-    void Loop(void)
+    void loop(void)
     {
-      tkr->mainloop();
+      for (int i = 0; i < num_subjects; i++)
+        vrpn_subjects[i]->loop();
     }
+
   private:
     ros::NodeHandle n;
-    string host_addr, tf_ref_frame, subjects, address;
-    vrpn_Tracker_Remote *tkr; 
+    string host_addr, tf_ref_frame, subjects[MAX_SUBJECTS], addresses[MAX_SUBJECTS];
+    int num_subjects;
+    VRPNSubject * vrpn_subjects[MAX_SUBJECTS]; 
     tf::TransformBroadcaster tf_broadcaster;
-
-    void VRPN_CALLBACK handle_tracker(void *userdata, const vrpn_TRACKERCB t)
-    {
-      //this function gets called when the tracker's POSITION xform is updated
-
-      //you can change what this callback function is called for by changing
-      //the type of t in the function prototype above.
-      //Options are:
-      //   vrpn_TRACKERCB              position
-      //   vrpn_TRACKERVELCB           velocity
-      //   vrpn_TRACKERACCCB           acceleration
-      //   vrpn_TRACKERTRACKER2ROOMCB  tracker2room transform 
-      //                                 (comes from local or remote
-      //                                  vrpn_Tracker.cfg file)
-      //   vrpn_TRACKERUNIT2SENSORCB   unit2sensor transform (see above comment)
-      //   vrpn_TRACKERWORKSPACECB     workspace bounding box (extent of tracker)
-
-      // userdata is whatever you passed into the register_change_handler function.
-      // vrpn sucks it up and spits it back out at you. It's not used by vrpn internally
-
-      ros::Time time_now;
-      static tf::TransformBroadcaster tf_broadcaster;
-      ROS_INFO("handle_tracker\tSensor %d is now at (%g,%g,%g)\n", t.sensor, t.pos[0], t.pos[1], t.pos[2]);
-      time_now = ros::Time::now();
-      tf::Transform transform;
-      transform.setOrigin(tf::Vector3(t.pos[0] / 1000, t.pos[1] / 1000, t.pos[2] / 1000));
-      transform.setRotation(tf::Quaternion(t.quat[0], t.quat[1], t.quat[2], t.quat[3]));
-      tf::StampedTransform stampTransform(transform, time_now, "/vicon_world", "Pioneer01");
-      tf_broadcaster.sendTransform(stampTransform);
-    }
 };
 
 //****************************************************************************
@@ -75,10 +63,10 @@ int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "vicon_vrpn_client");
 	ros::NodeHandle n;
-  VRPNHandler myHandler(n);
+  VRPNHandler my_handler(n);
 
   while (ros::ok()) 
   {
-		myHandler.Loop();
+		my_handler.loop();
   }
 }
