0f88cef8ed68b23903bde84e97d87e6ca070d9a5
titus.appel <titus.appel@localhost>
2011-02-22 15:46:22 -0700

    Changed some nav stack files

--- sandbox/nicola/pioneer_cmd_motor_state/src/main.cpp
+++ sandbox/nicola/pioneer_cmd_motor_state/src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv){
 
   ros::Rate r(1);
 
-  ros::Publisher motor_state_pub = n.advertise<p2os_driver::MotorState>("cmd_motor_state", 1);
+  ros::Publisher motor_state_pub = n.advertise<p2os_driver::MotorState>("/cmd_motor_state", 1);
 
   while(n.ok()){
     p2os_driver::MotorState state_msg;
--- sandbox/nicola/pioneer_setup_tf/src/tf_broadcaster.cpp
+++ sandbox/nicola/pioneer_setup_tf/src/tf_broadcaster.cpp
@@ -5,7 +5,7 @@ int main(int argc, char** argv){
   ros::init(argc, argv, "pioneer_tf_publisher");
   ros::NodeHandle n;
 
-  ros::Rate r(100);
+  ros::Rate r(50);
 
   tf::TransformBroadcaster broadcaster;
 
@@ -13,7 +13,7 @@ int main(int argc, char** argv){
     broadcaster.sendTransform(
       tf::StampedTransform(
         tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.1905, 0.0, 0.2659)),
-        ros::Time::now(),"base_link", "base_laser"));
+        ros::Time::now(),"/base_link", "/base_laser"));
     ros::spinOnce();
     r.sleep();
   }
