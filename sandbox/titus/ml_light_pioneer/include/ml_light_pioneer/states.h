0b147a7018d340c2937264d48a7574800cd2fcc7
titus.appel <titus.appel@localhost>
2011-05-17 14:34:33 -0600

    Added pioneer ml light

--- sandbox/titus/ml_light_pioneer/include/ml_light_pioneer/states.h
+++ sandbox/titus/ml_light_pioneer/include/ml_light_pioneer/states.h
@@ -21,8 +21,11 @@ private:
   ros::Subscriber sub_odom_, sub_flls_, sub_frls_, sub_rlls_, sub_rrls_;
   ros::Timer tmr_state_;
   int state_, num_states_;
-  double ls_vals_[4];
+  double ls_vals_[4], xdist_, ydist_, hyp_, cos_ang_, sin_ang_;
+  double light_dir_, light_dir_last_, ang_inc_, ang_start_;
 
+  void cb_tmr_state(const ros::TimerEvent& event);
+  void cb_odom(nav_msgs::Odometry msg);
   void cb_flls(phidgets_ros::Float64Stamped msg);
   void cb_frls(phidgets_ros::Float64Stamped msg);
   void cb_rlls(phidgets_ros::Float64Stamped msg);
