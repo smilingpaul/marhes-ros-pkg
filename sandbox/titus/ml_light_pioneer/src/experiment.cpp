2e5003adeb0cabb093063674c76e1321630502a5
titus.appel <titus.appel@localhost>
2011-06-24 14:25:02 -0600

    Added marhes map and changed some ml light code.

--- sandbox/titus/ml_light_pioneer/src/experiment.cpp
+++ sandbox/titus/ml_light_pioneer/src/experiment.cpp
@@ -7,12 +7,14 @@
 
 #include "ros/ros.h"
 #include "nav_msgs/Odometry.h"
+#include "nav_msgs/Path.h"
 #include "ml_light_pioneer/actions.h"
 #include "ml_light_pioneer/qlearner.h"
 #include "ml_light_pioneer/states.h"
 #include "ml_light_pioneer/learning_curve.h"
 #include "std_msgs/Bool.h"
 #include "geometry_msgs/Pose.h"
+#include "geometry_msgs/PoseStamped.h"
 
 class Experiment
 {
@@ -34,15 +36,17 @@ private:
 	QLearner* qobj_;
 	LearningCurve* lc_;
 	
-  ros::Publisher move_pub_;
+  ros::Publisher move_pub_, path_pub_;
 	ros::Subscriber bool_sub_, odom_sub_;
 	ros::Timer timer_;
+	nav_msgs::Path path_msg_;
 
 	void odom_cb(const nav_msgs::Odometry msg);
 	void bool_cb(const std_msgs::Bool msg);
 	void timer_cb(const ros::TimerEvent& event);
 	double getDistance(void);
 	bool outOfBounds(void);
+	void stopAndMoveToStart(void);
 };
 
 Experiment::Experiment(ros::NodeHandle n):n_(n)
@@ -74,7 +78,10 @@ Experiment::Experiment(ros::NodeHandle n):n_(n)
 	qobj_ = new QLearner(n);
 	lc_ = new LearningCurve();
 
+  path_msg_.header.frame_id = "odom";
+
   move_pub_ = n_.advertise<geometry_msgs::Pose>("move_cmd", 1);
+  path_pub_ = n_.advertise<nav_msgs::Path>("path", 1);
   bool_sub_ = n_.subscribe("move_done", 1, &Experiment::bool_cb, this);
 	odom_sub_ = n.subscribe("base_pose_ground_truth", 10, &Experiment::odom_cb, this);
 	timer_ = n.createTimer(ros::Duration(1/freq_), &Experiment::timer_cb, this);
@@ -82,7 +89,27 @@ Experiment::Experiment(ros::NodeHandle n):n_(n)
 
 void Experiment::odom_cb(const nav_msgs::Odometry msg)
 {
+  geometry_msgs::PoseStamped pose;
+
   odom_msg_ = msg;
+  
+  if (mode_ != MODE_RETURN)
+  {
+    pose.header.stamp = odom_msg_.header.stamp;
+    pose.header.frame_id = "odom";
+    pose.pose = odom_msg_.pose.pose;
+    path_msg_.header.stamp = odom_msg_.header.stamp;
+    path_msg_.poses.push_back(pose);
+
+    path_pub_.publish(path_msg_);
+  }
+  else
+  {
+    path_msg_.poses.clear();
+  }  
+  
+  if (outOfBounds() && (mode_ != MODE_RETURN))
+    stopAndMoveToStart();
 }
 
 void Experiment::bool_cb(const std_msgs::Bool msg)
@@ -124,27 +151,7 @@ void Experiment::timer_cb(const ros::TimerEvent& event)
 
 		if (getDistance() < goal_radius_ || outOfBounds())
 		{
-      double rand_ang, rand_orientation, start_x, start_y;
-
-			mode_ = MODE_RETURN;
-			ROS_INFO("Completed rep: %d, returning to start location", cnt_rep_); 
-			actions_->Stop();
-			lc_->UpdateSteps(cnt_timesteps_);
-			cnt_timesteps_ = 0;
-
-      // Calculate next position
-      rand_ang = 2.0 * M_PI * (rand() / ((double)RAND_MAX + 1));
-      rand_orientation = 2.0 * M_PI * (rand() / ((double)RAND_MAX + 1));
-      ROS_INFO("Rand_Ang: %f, Rand orient: %f", rand_ang, rand_orientation);
-      start_x = goalx_ + start_radius_ * cos(rand_ang);
-      start_y = goaly_ + start_radius_ * sin(rand_ang);
-      
-      // Send the next start position and wait for move_stopped flag
-      geometry_msgs::Pose start_msg;
-      start_msg.position.x = start_x;
-      start_msg.position.y = start_y;
-      start_msg.orientation = tf::createQuaternionMsgFromYaw(rand_orientation);
-      move_pub_.publish(start_msg);
+      stopAndMoveToStart();
 		}
 		break;
 	case MODE_RETURN:
@@ -175,10 +182,40 @@ double Experiment::getDistance(void)
 bool Experiment::outOfBounds(void)
 {
   double x = odom_msg_.pose.pose.position.x, y = odom_msg_.pose.pose.position.y;
+  //ROS_INFO("X: %f, Y: %f, B0: %f, B1: %f, B2: %f, B3: %f", x, y, bounds_[0], 
+  //          bounds_[1], bounds_[2], bounds_[3]);
   if (x < bounds_[0] || x > bounds_[2] || y > bounds_[1] || y < bounds_[3])
     return true;
   else
     return false;
+}
+
+void Experiment::stopAndMoveToStart(void)
+{
+  double rand_ang, rand_orientation, start_x, start_y;
+
+  double x = odom_msg_.pose.pose.position.x, y = odom_msg_.pose.pose.position.y;
+  ROS_INFO("X: %f, Y: %f, B0: %f, B1: %f, B2: %f, B3: %f", x, y, bounds_[0], 
+           bounds_[1], bounds_[2], bounds_[3]);
+	mode_ = MODE_RETURN;
+	ROS_INFO("Completed rep: %d, returning to start location", cnt_rep_); 
+	actions_->Stop();
+	lc_->UpdateSteps(cnt_timesteps_);
+	cnt_timesteps_ = 0;
+
+  // Calculate next position
+  rand_ang = 2.0 * M_PI * (rand() / ((double)RAND_MAX + 1));
+  rand_orientation = 2.0 * M_PI * (rand() / ((double)RAND_MAX + 1));
+  ROS_INFO("Rand_Ang: %f, Rand orient: %f", rand_ang, rand_orientation);
+  start_x = goalx_ + start_radius_ * cos(rand_ang);
+  start_y = goaly_ + start_radius_ * sin(rand_ang);
+  
+  // Send the next start position and wait for move_stopped flag
+  geometry_msgs::Pose start_msg;
+  start_msg.position.x = start_x;
+  start_msg.position.y = start_y;
+  start_msg.orientation = tf::createQuaternionMsgFromYaw(rand_orientation);
+  move_pub_.publish(start_msg);
 }
 
 int main(int argc, char **argv)
