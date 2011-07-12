2871255c23494b64ec93e6763f46f273cf937fd5
titus.appel <titus.appel@localhost>
2011-07-11 22:30:23 -0600

    Added vicon_sdk and updated txt_driver for external odom fix and extras

--- txt1/txt_driver/src/txt1.cpp
+++ txt1/txt_driver/src/txt1.cpp
@@ -14,15 +14,24 @@ TXT1::TXT1(ros::NodeHandle nh)
 
 	std::string def = "/dev/ttyUSB0";
 	n_private.param("port", port_, def);
-	COMB_ODOM_CNT_LIMIT_ = 1;
+	n_private.param("pwr_auto", pwr_auto_, false);
+	// TODO: Add PID Gains Parameter table
+	
+	// Initialize variables
 	shutdown_ = false;
 
+  // Initialize the subscribers
 	cmd_vel_sub_ = n_.subscribe("/cmd_vel", 1, &TXT1::cmdVelCB, this);
-//	comb_odom_sub_ = n_.subscribe("/robot_pose_ekf/odom_combined", 1000, &TXT1::combOdomCB, this);
-	comb_odom_sub_ = n_.subscribe("/vo", 1, &TXT1::combOdomCB, this);
+	
+  // Added 'no delay' option because it was buffering every two data packets and 
+  // sending old data
+	comb_odom_sub_ = n_.subscribe<nav_msgs::Odometry>("/vo", 100, 
+	    &TXT1::combOdomCB, this, ros::TransportHints().tcpNoDelay());
+	pwm_sub_ = n_.subscribe<txt_driver::Pwm>("/pwm", 10, &TXT1::pwmMsgCB, this);
 
 	cmd_vel_tmr_ = n_.createTimer(ros::Duration(0.1), &TXT1::cmdVelTmrCB, this);
-	comb_odom_tmr_ = n_.createTimer(ros::Duration(0.05), &TXT1::combOdomTmrCB, this);
+	comb_odom_tmr_ = n_.createTimer(ros::Duration(1.0), &TXT1::combOdomTmrCB, 
+	    this);
 
 	odom_pub_ = n_.advertise<nav_msgs::Odometry>("/odom", 50);
 	battery_pub_ = n_.advertise<txt_driver::Battery>("/battery", 50);
@@ -30,14 +39,14 @@ TXT1::TXT1(ros::NodeHandle nh)
 
 	pid_srv_ = n_.advertiseService("/pid_change", &TXT1::pidSrvCB, this);
   shutdown_srv_ = n_.advertiseService("/shutdown_computer", &TXT1::shutdownSrvCB, this);
-  pwm_srv_ = n_.advertiseService("/pwm_change", &TXT1::pwmSetValsCB, this);
+  pwm_test_srv_ = n_.advertiseService("/pwm_change", &TXT1::pwmSetValsCB, this);
+  switch_pwr_srv_ = n_.advertiseService("/switched_pwr", &TXT1::switchPwrCB, this);
 
-	// Added for testing
+	// Initialize combined odometry message
 	comb_odom_msg_.header.stamp = ros::Time::now();
 	comb_odom_msg_.pose.pose.orientation = tf::createQuaternionMsgFromYaw(0.0);
-//	comb_odom_msg_.twist.twist.linear.x = 0.0;
-//	comb_odom_msg_.twist.twist.angular.z = 0.0;
 
+  // Open serial port @ 57600 baud 8N1
 	my_serial_ = new Serial(port_, Serial::BAUD_57600, Serial::SIZE_8,
 			Serial::NONE, Serial::ONE);
 
@@ -78,39 +87,34 @@ void TXT1::cmdVelTmrCB(const ros::TimerEvent& e)
 
 void TXT1::combOdomTmrCB(const ros::TimerEvent& e)
 {
-	if (comb_odom_cnt_ > 0)//COMB_ODOM_CNT_LIMIT_)
+	if (comb_odom_cnt_ > COMB_ODOM_CNT_LIMIT_)
 	{
-		Packet packet;
-		packet.BuildCombOdom(comb_odom_msg_);
-		packet.Send(my_serial_);
-		ROS_INFO("Sent COMB ODOM");
+    use_comb_odom_ = true;
 	}
 	else
 	{
-	  ROS_INFO("No Messages Available");
+	  use_comb_odom_ = false;
 	}
-
 	comb_odom_cnt_ = 0;
 }
-//void TXT1::combOdomCB(const geometry_msgs::PoseWithCovarianceStampedConstPtr &msg)
-void TXT1::combOdomCB(const nav_msgs::OdometryConstPtr &msg)
+
+void TXT1::combOdomCB(nav_msgs::Odometry msg)
 {
-  ROS_INFO("Rx VO");
-	comb_odom_cnt_++;
-	comb_odom_msg_ = *msg;
-//	ros::Duration dt = msg->header.stamp - comb_pose_msg_.header.stamp;
-//	double vx = sqrt(pow(msg->pose.pose.position.x -
-//		  comb_pose_msg_.pose.pose.position.x, 2) +
-//	      pow(msg->pose.pose.position.y - comb_pose_msg_.pose.pose.position.y, 2))
-//	      / dt.toSec();
-//	double vt = (tf::getYaw(msg->pose.pose.orientation) -
-//			tf::getYaw(comb_pose_msg_.pose.pose.orientation)) / dt.toSec();
-//
-//	comb_pose_msg_ = *msg;
-//	comb_odom_msg_.header.stamp = ros::Time::now();
-//	comb_odom_msg_.pose.pose = comb_pose_msg_.pose.pose;
-//	comb_odom_msg_.twist.twist.linear.x = vx;
-//	comb_odom_msg_.twist.twist.angular.z = vt;
+  comb_odom_cnt_++;
+  if (use_comb_odom_ == true)
+  {
+	  comb_odom_msg_ = msg;
+	  Packet packet;
+		packet.BuildCombOdom(comb_odom_msg_);
+		packet.Send(my_serial_);
+	}
+}
+
+void TXT1::pwmMsgCB(txt_driver::Pwm msg)
+{
+  Packet packet;
+  packet.BuildPwm(msg);
+  packet.Send(my_serial_);
 }
 
 void TXT1::pubOdom(double x, double y, double theta, double vx, double vtheta)
@@ -122,42 +126,42 @@ void TXT1::pubOdom(double x, double y, double theta, double vx, double vtheta)
 	odom_trans_msg_.header.frame_id = "/odom";
 	odom_trans_msg_.child_frame_id = "/base_footprint";
 
-    odom_trans_msg_.transform.translation.x = x;
-    odom_trans_msg_.transform.translation.y = y;
-    odom_trans_msg_.transform.translation.z = 0.0;
-    odom_quat_ = tf::createQuaternionMsgFromYaw(theta);
-    odom_trans_msg_.transform.rotation = odom_quat_;
+  odom_trans_msg_.transform.translation.x = x;
+  odom_trans_msg_.transform.translation.y = y;
+  odom_trans_msg_.transform.translation.z = 0.0;
+  odom_quat_ = tf::createQuaternionMsgFromYaw(theta);
+  odom_trans_msg_.transform.rotation = odom_quat_;
 
-    //send the transform
-    odom_broadcaster_.sendTransform(odom_trans_msg_);
+  //send the transform
+  odom_broadcaster_.sendTransform(odom_trans_msg_);
 
-    //next, we'll publish the odometry message over ROS
-    odom_msg_.header.stamp = currentTime;
-    odom_msg_.header.frame_id = "/odom";
+  //next, we'll publish the odometry message over ROS
+  odom_msg_.header.stamp = currentTime;
+  odom_msg_.header.frame_id = "/odom";
 
-    //set the position
-    odom_msg_.pose.pose.position.x = x;
-    odom_msg_.pose.pose.position.y = y;
-    odom_msg_.pose.pose.position.z = 0.0;
-    odom_msg_.pose.pose.orientation = odom_quat_;
+  //set the position
+  odom_msg_.pose.pose.position.x = x;
+  odom_msg_.pose.pose.position.y = y;
+  odom_msg_.pose.pose.position.z = 0.0;
+  odom_msg_.pose.pose.orientation = odom_quat_;
 
-    /// set the position covariance
-    odom_msg_.pose.covariance[0] = 1.0;
-    odom_msg_.pose.covariance[7] = 1.0;
-    odom_msg_.pose.covariance[14] = 99999;
-    odom_msg_.pose.covariance[21] = 99999;
-    odom_msg_.pose.covariance[28] = 99999;
-    odom_msg_.pose.covariance[35] = 0.5;
+  /// set the position covariance
+  odom_msg_.pose.covariance[0] = 1.0;
+  odom_msg_.pose.covariance[7] = 1.0;
+  odom_msg_.pose.covariance[14] = 99999;
+  odom_msg_.pose.covariance[21] = 99999;
+  odom_msg_.pose.covariance[28] = 99999;
+  odom_msg_.pose.covariance[35] = 0.5;
 
-    //set the velocity
-    odom_msg_.child_frame_id = "/base_footprint";
-    odom_msg_.twist.twist.linear.x = vx;
-    odom_msg_.twist.twist.linear.y = 0.0;
-    odom_msg_.twist.twist.linear.z = 0.0;
-    odom_msg_.twist.twist.angular.z = vtheta;
+  //set the velocity
+  odom_msg_.child_frame_id = "/base_footprint";
+  odom_msg_.twist.twist.linear.x = vx;
+  odom_msg_.twist.twist.linear.y = 0.0;
+  odom_msg_.twist.twist.linear.z = 0.0;
+  odom_msg_.twist.twist.angular.z = vtheta;
 
-    //publish the message
-    odom_pub_.publish(odom_msg_);
+  //publish the message
+  odom_pub_.publish(odom_msg_);
 }
 
 void TXT1::pubBattery(double batt1, double batt2)
@@ -181,10 +185,8 @@ void TXT1::pubPidTerms(double pterm, double iterm, double dterm, double signal)
   pid_terms_pub_.publish(msg);  
 }
 
-bool TXT1::pidSrvCB(txt_driver::pid::Request& request, txt_driver::pid::Response& response)
+bool TXT1::pidSrvCB(txt_driver::Pid::Request& request, txt_driver::Pid::Response& response)
 {
-	pid_req_ = request;
-
 	Packet packet;
 	packet.BuildPidTx(request);
 	packet.Send(my_serial_);
@@ -193,22 +195,30 @@ bool TXT1::pidSrvCB(txt_driver::pid::Request& request, txt_driver::pid::Response
 	return true;
 }
 
-bool TXT1::pwmSetValsCB(txt_driver::pwm::Request& request, txt_driver::pwm::Response& response)
+bool TXT1::pwmSetValsCB(txt_driver::PwmTest::Request& request, txt_driver::PwmTest::Response& response)
 {
-  pwm_req_ = request;
-  
   Packet packet;
-  packet.BuildPWMTx(request);
+  packet.BuildPWMTestTx(request);
   packet.Send(my_serial_);
   
   response.result = true;
   return true;
 }
 
-bool TXT1::shutdownSrvCB(txt_driver::shutdown::Request& request, txt_driver::shutdown::Response& response)
+bool TXT1::shutdownSrvCB(txt_driver::Shutdown::Request& request, txt_driver::Shutdown::Response& response)
 {
   shutdown_ = true;
   ros::shutdown();
+  response.result = true;
+  return true;
+}
+
+bool TXT1::switchPwrCB(txt_driver::SwitchedPwr::Request& request, txt_driver::SwitchedPwr::Response& response)
+{
+  Packet packet;
+  packet.BuildSwitchPwr(request);
+  packet.Send(my_serial_);
+  
   response.result = true;
   return true;
 }
@@ -226,11 +236,16 @@ int main(int argc, char **argv)
 
   while (ros::ok())
   {
-    rxPacket.Receive(p->my_serial_);
+    //rxPacket.Receive(p->my_serial_);
 
     ros::spinOnce();
     loop_rate.sleep();
   }
+  
+  //ros::MultiThreadedSpinner spinner(4); // Use 4 threads
+  //spinner.spin(); // spin() will not return until the node has been shutdown
+  
+  //ros::spin();
   
   // Do this to make sure the computer shuts down when batteries are bad
   // Modify your /etc/sudoers file by adding a line like this:-
---
+++ vicon/vicon_sdk/src/vicon_sdk.cpp
@@ -0,0 +1,115 @@
+#include "ros/ros.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "vicon_sdk/Client.h"
+
+using namespace ViconDataStreamSDK::CPP;
+
+class ViconToPose
+{
+public:
+  ViconToPose(ros::NodeHandle nh);
+private:
+  ros::NodeHandle n_;
+  ros::Publisher pose_pub_;
+  double freq_;
+  std::string subject_, server_, subjectName_, rootSegment_;
+  ros::Timer calc_tmr_;
+  Client vicon_client_;
+  
+  void ViconTmr(const ros::TimerEvent& event);
+};
+
+ViconToPose::ViconToPose(ros::NodeHandle nh)
+{
+  n_ = nh;
+  ros::NodeHandle n_private("~");
+  n_private.param("freq", freq_, 50.0);
+  n_private.param("server", server_, std::string("Vicon"));
+  
+  if (!n_private.hasParam("subject"))
+  {
+    ROS_FATAL("Please specify target frame to track!");
+    exit(1);
+  }
+  
+  // Get the subject to track
+  n_private.getParam("subject", subject_);
+  
+  // Try to connect to the server
+  while(ros::ok() && !vicon_client_.IsConnected().Connected)
+  {
+    vicon_client_.Connect(server_);
+    ROS_INFO("Waiting to connect.");
+    ros::Duration(1).sleep();
+  }
+  ROS_INFO("Connected to Vicon Server at %s", server_.c_str());
+  
+  vicon_client_.EnableSegmentData();
+  vicon_client_.DisableMarkerData();
+  vicon_client_.DisableUnlabeledMarkerData();
+  vicon_client_.DisableDeviceData();
+   
+  vicon_client_.SetStreamMode( ViconDataStreamSDK::CPP::StreamMode::ClientPull );
+  //vicon_client_.SetStreamMode( ViconDataStreamSDK::CPP::StreamMode::ClientPullPreFetch );
+  //vicon_client_.SetStreamMode( ViconDataStreamSDK::CPP::StreamMode::ServerPush );
+  
+  // Set the global up axis
+  vicon_client_.SetAxisMapping(Direction::Forward, Direction::Left, Direction::Up);
+  
+  unsigned int subjectCount = vicon_client_.GetSubjectCount().SubjectCount;
+  for( unsigned int subjectIndex = 0 ; subjectIndex < subjectCount ; ++subjectIndex )
+  {
+    // Get the subject name
+    std::string subjectName = vicon_client_.GetSubjectName(subjectIndex).SubjectName;
+    // Get the root segment
+    std::string rootSegment = vicon_client_.GetSubjectRootSegmentName(subjectName).SegmentName;
+    
+    if (subjectName == subject_)
+    {
+      subjectName_ = subjectName;
+      rootSegment_ = rootSegment;
+      break;
+    }
+  }
+  
+  pose_pub_ = n_.advertise<geometry_msgs::PoseStamped>("pose", 1);
+  calc_tmr_ = n_.createTimer(ros::Duration(1/freq_), &ViconToPose::ViconTmr, this);
+}
+
+void ViconToPose::ViconTmr(const ros::TimerEvent& event)
+{
+  if(vicon_client_.GetFrame().Result != Result::Success)
+  {
+    geometry_msgs::PoseStamped msg;
+    msg.header.stamp = ros::Time::now();
+    msg.header.frame_id = "/odom";
+  
+    Output_GetSegmentGlobalTranslation _Output_GetSegmentGlobalTranslation = 
+        vicon_client_.GetSegmentGlobalTranslation(subjectName_, rootSegment_);
+    msg.pose.position.x = _Output_GetSegmentGlobalTranslation.Translation[0];
+    msg.pose.position.y = _Output_GetSegmentGlobalTranslation.Translation[1];
+    msg.pose.position.z = _Output_GetSegmentGlobalTranslation.Translation[2];
+    
+    Output_GetSegmentGlobalRotationQuaternion _Output_GetSegGlobalQuaternion = 
+        vicon_client_.GetSegmentGlobalRotationQuaternion(subjectName_, rootSegment_);
+    msg.pose.orientation.x = _Output_GetSegGlobalQuaternion.Rotation[0];
+    msg.pose.orientation.y = _Output_GetSegGlobalQuaternion.Rotation[1];
+    msg.pose.orientation.z = _Output_GetSegGlobalQuaternion.Rotation[2];
+    msg.pose.orientation.w = _Output_GetSegGlobalQuaternion.Rotation[3];
+    
+    pose_pub_.publish(msg);
+  }
+}
+
+int main(int argc, char **argv)
+{
+  // Initial ROS setup
+	ros::init(argc, argv, "vicon_to_pose");
+	ros::NodeHandle n;
+
+  ViconToPose* translater = new ViconToPose(n);
+
+  ros::spin();
+
+  return 0;
+}
