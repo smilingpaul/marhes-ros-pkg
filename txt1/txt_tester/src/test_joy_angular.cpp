e00df7688ddb50370775524504e84b5a2639aaf8
titus.appel <titus.appel@localhost>
2011-07-19 21:16:23 -0600

    Fixed the txt_tester to have appropiate ang vel with min radius

--- txt1/txt_tester/src/test_joy_angular.cpp
+++ txt1/txt_tester/src/test_joy_angular.cpp
@@ -2,6 +2,7 @@
 #include "joy/Joy.h"
 #include "geometry_msgs/Twist.h"
 #include "std_msgs/Float64.h"
+#include <cmath>
 
 class Txt1Teleop{
 public:
@@ -20,7 +21,7 @@ private:
 	joy::Joy joyMsg;
 
 	int linAxis, angAxis;
-	double linVel, ang_vel_max;
+	double linVel, lin_vel_max, ang_vel_max, radius_min;
 	bool joyRxFlag;
 };
 
@@ -30,9 +31,11 @@ Txt1Teleop::Txt1Teleop(ros::NodeHandle nh):n(nh)
 
 	n_private.param("linAxis", linAxis, 1);
 	n_private.param("angAxis", angAxis, 0);
-	n_private.param("angVelMax", ang_vel_max, 2.0);
+	n_private.param("linVelMax", lin_vel_max, 1.0);
+	n_private.param("radiusMin", radius_min, 0.555);
 	
 	joyRxFlag = false;
+	ang_vel_max = 0.0;
 
 	velPub = n.advertise<geometry_msgs::Twist>("cmd_vel",1);
 	joySub = n.subscribe<joy::Joy>("joy", 10, &Txt1Teleop::joyCB, this);
@@ -51,7 +54,12 @@ void Txt1Teleop::cmdVelTmrCB(const ros::TimerEvent& e)
   geometry_msgs::Twist vel;
 	if (joyRxFlag && joyMsg.buttons[0] == 1)
   {
+    if (linVel > lin_vel_max)
+      linVel = lin_vel_max;
+    else if (linVel < -lin_vel_max)
+      linVel = -lin_vel_max;
     vel.linear.x = linVel;
+    ang_vel_max = std::abs(linVel) / radius_min;
     vel.angular.z = ang_vel_max * joyMsg.axes[angAxis];
   }
   else
